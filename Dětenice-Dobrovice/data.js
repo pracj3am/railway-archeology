aa85fa027dbd03305d5d654e238fbb0322931fc6
Jan Prachař <jan.prachar@gmail.com>
2013-09-01 04:23:57 +0200

    Dětenice-Dobrovice mapa

---
+++ Dětenice-Dobrovice/data.js
@@ -0,0 +1,310 @@
+data = [
+    {
+        "photo": "2013-08-24 11.08.18 HDR.jpg",
+        "datetime": "2013:08:24 11:08:18",
+        "latitude": 50.368,
+        "longitude": 15.13417,
+        "title": "Zastávka Rokytňany, trať do Dobrovice pokračovala přímo"
+    },
+    {
+        "photo": "2013-08-24 11.14.25 HDR.jpg",
+        "datetime": "2013:08:24 11:14:25",
+        "latitude": 50.36783,
+        "longitude": 15.13333,
+        "title": "Podpěra drátovodu, který vedl k návěstidlu, kousek od zastávky Rokytňany"
+    },
+    {
+        "photo": "2013-08-24 11.24.59 HDR.jpg",
+        "datetime": "2013:08:24 11:24:59",
+        "latitude": 50.367,
+        "longitude": 15.13067,
+        "title": "Návěstidlo, které krylo odbočku směrem od Dobrovice"
+    },
+    {
+        "photo": "2013-08-24 11.36.50 HDR.jpg",
+        "datetime": "2013:08:24 11:36:50",
+        "latitude": 50.36417,
+        "longitude": 15.12967,
+        "title": "Největší zachovalý most na trati"
+    },
+    {
+        "photo": "2013-08-24 11.52.01 HDR.jpg",
+        "datetime": "2013:08:24 11:52:01",
+        "latitude": 50.36067,
+        "longitude": 15.13183,
+        "title": "Až do Ujkovic vede po tělese trati lesní cesta"
+    },
+    {
+        "photo": "2013-08-24 11.56.48 HDR.jpg",
+        "datetime": "2013:08:24 11:56:48",
+        "latitude": 50.359,
+        "longitude": 15.13083,
+        "title": "Pozůstatek stožáru při trati"
+    },
+    {
+        "photo": "2013-08-24 12.09.34 HDR.jpg",
+        "datetime": "2013:08:24 12:09:34",
+        "latitude": 50.35917,
+        "longitude": 15.12333,
+        "title": "Zbylý pražec na lesní cestě"
+    },
+    {
+        "photo": "2013-08-24 12.20.56 HDR.jpg",
+        "datetime": "2013:08:24 12:20:56",
+        "latitude": 50.357,
+        "longitude": 15.118,
+        "title": "Propustek"
+    },
+    {
+        "photo": "2013-08-24 12.24.14 HDR.jpg",
+        "datetime": "2013:08:24 12:24:14",
+        "latitude": 50.35783,
+        "longitude": 15.11667,
+        "title": "Kilometrovník"
+    },
+    {
+        "photo": "2013-08-24 12.33.35 HDR.jpg",
+        "datetime": "2013:08:24 12:33:35",
+        "latitude": 50.36083,
+        "longitude": 15.112,
+        "title": "Několik pražců na lesní cestě"
+    },
+    {
+        "photo": "2013-08-24 12.40.14 HDR.jpg",
+        "datetime": "2013:08:24 12:40:14",
+        "latitude": 50.361,
+        "longitude": 15.107,
+        "title": "Pozůstatky přejezdu na silnici 279 u Ujkovic (směrem na Dobrovici)"
+    },
+    {
+        "photo": "2013-08-24 13.09.56 HDR.jpg",
+        "datetime": "2013:08:24 13:09:56",
+        "latitude": 50.36383,
+        "longitude": 15.09833,
+        "title": "Druhý bývalý přejezd v Ujkovicích směrem na Dobrovici a býv. nádraží Ujkovice, které je nyní v soukromém vlastnictví, jak varuje cedule"
+    },
+    {
+        "photo": "2013-08-24 13.14.11 HDR.jpg",
+        "datetime": "2013:08:24 13:14:11",
+        "latitude": 50.36383,
+        "longitude": 15.09833,
+        "title": "Pohled zpět od přejezdu k Dětenicím (býv. těleso je travnaté za plotem)"
+    },
+    {
+        "photo": "2013-08-24 13.38.06 HDR.jpg",
+        "datetime": "2013:08:24 13:38:06",
+        "latitude": 50.36267,
+        "longitude": 15.086,
+        "title": "V místě pískovny již po trati nejsou památky, zde se těleso opět objevuje na kraji lesa (pražec a kilometrovník vlevo)"
+    },
+    {
+        "photo": "2013-08-24 13.38.20 HDR.jpg",
+        "datetime": "2013:08:24 13:38:20",
+        "latitude": 50.36283,
+        "longitude": 15.086,
+        "title": "Detail kilometrovníku"
+    },
+    {
+        "photo": "2013-08-24 13.42.13 HDR.jpg",
+        "datetime": "2013:08:24 13:42:13",
+        "latitude": 50.36217,
+        "longitude": 15.08417,
+        "title": "Těleso dále pokračuje lesem na vysokých náspech i v hlubokých zářezech, zůstalo zde velké množství pražců"
+    },
+    {
+        "photo": "2013-08-24 13.53.29 HDR.jpg",
+        "datetime": "2013:08:24 13:53:29",
+        "latitude": 50.361,
+        "longitude": 15.07833,
+        "title": "Křížení tělesa s lesní cestou"
+    },
+    {
+        "photo": "2013-08-24 13.57.47 HDR.jpg",
+        "datetime": "2013:08:24 13:57:47",
+        "latitude": 50.361,
+        "longitude": 15.07817,
+        "title": "Propustek s poškozeným tělesem (pravd. ho teprve letos odnesla velká voda)"
+    },
+    {
+        "photo": "2013-08-24 14.10.12 HDR.jpg",
+        "datetime": "2013:08:24 14:10:12",
+        "latitude": 50.3625,
+        "longitude": 15.075,
+        "title": "Kilometrovník"
+    },
+    {
+        "photo": "2013-08-24 14.11.02.jpg",
+        "datetime": "2013:08:24 14:11:02",
+        "latitude": 50.3625,
+        "longitude": 15.07483,
+        "title": "Krátký lépe průchodný úsek s viditelným pražcem, dále k Dobrovici je těleso čím dál víc zarostlé"
+    },
+    {
+        "photo": "2013-08-24 14.17.53 HDR.jpg",
+        "datetime": "2013:08:24 14:17:53",
+        "latitude": 50.36283,
+        "longitude": 15.073,
+        "title": "Džungle pokračuje I po té, co trať přejde z lesa na louku"
+    },
+    {
+        "photo": "2013-08-24 14.54.07 HDR.jpg",
+        "datetime": "2013:08:24 14:54:07",
+        "latitude": 50.365,
+        "longitude": 15.05817,
+        "title": "Jedno ze dvou míst, kde se dochovaly kolejnice"
+    },
+    {
+        "photo": "2013-08-24 14.54.22 HDR.jpg",
+        "datetime": "2013:08:24 14:54:22",
+        "latitude": 50.365,
+        "longitude": 15.058,
+        "title": "Pohled zpátky k Dětenicím, trať vede dále pořád podél silnice až do Žerčic"
+    },
+    {
+        "photo": "2013-08-24 14.54.38 HDR.jpg",
+        "datetime": "2013:08:24 14:54:38",
+        "latitude": 50.365,
+        "longitude": 15.058,
+        "title": "Detail kolejnice"
+    },
+    {
+        "photo": "2013-08-24 14.57.42 HDR.jpg",
+        "datetime": "2013:08:24 14:57:42",
+        "latitude": 50.3655,
+        "longitude": 15.05533,
+        "title": "Těleso vedoucí podél silnice je naprosto neschůdné, zde křížení s odbočkou na pole a jeden pražec"
+    },
+    {
+        "photo": "2013-08-24 14.58.01 HDR.jpg",
+        "datetime": "2013:08:24 14:58:01",
+        "latitude": 50.3655,
+        "longitude": 15.0555,
+        "title": "Pohled z druhé strany (tj. směrem k Dobrovici)"
+    },
+    {
+        "photo": "2013-08-24 15.14.34 HDR.jpg",
+        "datetime": "2013:08:24 15:14:34",
+        "latitude": 50.37033,
+        "longitude": 15.04,
+        "title": "Dávno po zrušení trati rekonstruovaný přejezd (v r. 1992) u Žerčic (pohled zpátky k Dětenicím)"
+    },
+    {
+        "photo": "2013-08-24 15.21.18 HDR.jpg",
+        "datetime": "2013:08:24 15:21:18",
+        "latitude": 50.37083,
+        "longitude": 15.03467,
+        "title": "Mostek v Žeržicích dnes slouží jako přístupová cesta na pole (pohled zpátky k Dětenicím)"
+    },
+    {
+        "photo": "2013-08-24 15.23.59 HDR.jpg",
+        "datetime": "2013:08:24 15:23:59",
+        "latitude": 50.37083,
+        "longitude": 15.03483,
+        "title": "Pohled z mostku směrem býv. nádraží Žerčice"
+    },
+    {
+        "photo": "2013-08-24 15.26.36 HDR.jpg",
+        "datetime": "2013:08:24 15:26:36",
+        "latitude": 50.37083,
+        "longitude": 15.03267,
+        "title": "Bývalá výpravní budova nádraží Žeržice (srv. <a href='http://www.pshzd.cz/archeo/dobrovice/zercice_vlak.jpg'>foto z doby provozu</a>)"
+    },
+    {
+        "photo": "2013-08-24 15.27.26 HDR.jpg",
+        "datetime": "2013:08:24 15:27:26",
+        "latitude": 50.37067,
+        "longitude": 15.0325,
+        "title": "Prostor bývalého nádraží Žeržice (pohled zpátky k Dětenicím)"
+    },
+    {
+        "photo": "2013-08-24 15.44.23 HDR.jpg",
+        "datetime": "2013:08:24 15:44:23",
+        "latitude": 50.36733,
+        "longitude": 15.02133,
+        "title": "Těleso trati pokračuje dále po náspu mezi poli až na krátké výjimky neschůdné, zde krátký průchodný úsek u posedu se zbytky pražců"
+    },
+    {
+        "photo": "2013-08-24 16.04.16 HDR.jpg",
+        "datetime": "2013:08:24 16:04:16",
+        "latitude": 50.36517,
+        "longitude": 15.00717,
+        "title": "Mostek v Semčicích (pohled zpátky k Dětenicím)"
+    },
+    {
+        "photo": "2013-08-24 16.04.37 HDR.jpg",
+        "datetime": "2013:08:24 16:04:37",
+        "latitude": 50.36517,
+        "longitude": 15.00717,
+        "title": "Mostek z jiného úhlu"
+    },
+    {
+        "photo": "2013-08-24 16.11.36 HDR.jpg",
+        "datetime": "2013:08:24 16:11:36",
+        "latitude": 50.36467,
+        "longitude": 15.004,
+        "title": "Zbytky nakládací rampy nákladiště Semčice"
+    },
+    {
+        "photo": "2013-08-24 16.12.20 HDR.jpg",
+        "datetime": "2013:08:24 16:12:20",
+        "latitude": 50.36467,
+        "longitude": 15.00367,
+        "title": "Prostor bývalého nákladiště Semčice (pohled zpět k Dětenicím, srv. <a href='http://www.pshzd.cz/archeo/dobrovice/semcice99.jpg'>foto z roku 1999</a>)"
+    },
+    {
+        "photo": "2013-08-24 16.13.54 HDR.jpg",
+        "datetime": "2013:08:24 16:13:54",
+        "latitude": 50.3645,
+        "longitude": 15.0035,
+        "title": "Z přejezdu u nákladiště Semčice se již nic nezachovalo, pouze mostek a brána vlečky za silnicí"
+    },
+    {
+        "photo": "2013-08-24 16.16.49 HDR.jpg",
+        "datetime": "2013:08:24 16:16:49",
+        "latitude": 50.3645,
+        "longitude": 15.0035,
+        "title": "Návěst stoupání tratě o kousek dál"
+    },
+    {
+        "photo": "2013-08-24 16.20.13 HDR.jpg",
+        "datetime": "2013:08:24 16:20:13",
+        "latitude": 50.36433,
+        "longitude": 15.00217,
+        "title": "Zde zřejmě končily koleje v době, kdy je využívala Škoda pro odstavování vozů"
+    },
+    {
+        "photo": "2013-08-24 16.21.09 HDR.jpg",
+        "datetime": "2013:08:24 16:21:09",
+        "latitude": 50.36433,
+        "longitude": 15.002,
+        "title": "Značka z druhé strany (směrem zpátky k Dětenicím)"
+    },
+    {
+        "photo": "2013-08-24 16.36.10 HDR.jpg",
+        "datetime": "2013:08:24 16:36:10",
+        "latitude": 50.36483,
+        "longitude": 14.99167,
+        "title": "Nejzachovalejší těleso trati dále pokračuje mezi poli místy schůdné místy ne kvůli trnitým keřům"
+    },
+    {
+        "photo": "2013-08-24 16.54.05 HDR.jpg",
+        "datetime": "2013:08:24 16:54:05",
+        "latitude": 50.36333,
+        "longitude": 14.97967,
+        "title": "Zde byl násep rozbořen pravděpodobně kvůli letošním povodním"
+    },
+    {
+        "photo": "2013-08-24 17.09.04 HDR.jpg",
+        "datetime": "2013:08:24 17:09:04",
+        "latitude": 50.36117,
+        "longitude": 14.97517,
+        "title": "Odtud v místě trati vede cesta, na fotografii je pohled zpátky k Dětenicím, trať odbočovala od cesty a vedla směrem ke keřům"
+    },
+    {
+        "photo": "2013-08-24 17.14.12 HDR.jpg",
+        "datetime": "2013:08:24 17:14:12",
+        "latitude": 50.36,
+        "longitude": 14.97133,
+        "title": "Konec zrušeného úseku, zbytek trati až do stanice Dobrovice je využívan jako vlečka (včetně odbočky s trianglem)"
+    }
+]
