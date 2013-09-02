36afae72cb005dec76e65b807c8edd1350257770
Jan Prachař <jan.prachar@gmail.com>
2013-09-02 02:17:53 +0200

    Přidáno Zvoleněves-Kladno Dubí

---
+++ Zvoleněves-Kladno Dubí/data.js
@@ -0,0 +1,170 @@
+data = [
+    {
+        "photo": "IMG_1437.JPG",
+        "datetime": "2012:09:16 11:42:55",
+        "latitude": 50.2305253,
+        "longitude": 14.1835356,
+        "title": "Stanice Zvoleněves, ze které zrušená trať odbočovala"
+    },
+    {
+        "photo": "IMG_1438.JPG",
+        "datetime": "2012:09:16 12:20:06",
+        "latitude": 50.2249256,
+        "longitude": 14.1650347,
+        "title": "Na úseku až do začátku obce Podlešín byla postavena cyklostezka"
+    },
+    {
+        "photo": "IMG_1439.JPG",
+        "datetime": "2012:09:16 12:28:42",
+        "latitude": 50.2213078,
+        "longitude": 14.1597253,
+        "title": "Obcí Podlešín vede stoupou trati cesta, zde je za silnicí zachycena budova bývalé zastávky"
+    },
+    {
+        "photo": "IMG_1441.JPG",
+        "datetime": "2012:09:16 12:34:40",
+        "latitude": 50.2204133,
+        "longitude": 14.1584025,
+        "title": "Na dalším křížení s obecní komunikací se dochovaly koleje"
+    },
+    {
+        "photo": "IMG_1442.JPG",
+        "datetime": "2012:09:16 12:38:01",
+        "latitude": 50.2198081,
+        "longitude": 14.1569447,
+        "title": "Dochovaný propustek v obci Podlešín"
+    },
+    {
+        "photo": "IMG_1445.JPG",
+        "datetime": "2012:09:16 12:47:16",
+        "latitude": 50.2199239,
+        "longitude": 14.1518331,
+        "title": "Podlešínský viadukt, zrušená trať vedla druhým obloukem zleva"
+    },
+    {
+        "photo": "IMG_1449.JPG",
+        "datetime": "2012:09:16 13:38:04",
+        "latitude": 50.2139778,
+        "longitude": 14.1376353,
+        "title": "Dále se trať ztrácí v poli, těleso se objevuje v lese u obce Knovíz, vede po něm i cesta, která je zachycena na této fotografii (pohled směrem zpět k Podlešínu)"
+    },
+    {
+        "photo": "IMG_1451.JPG",
+        "datetime": "2012:09:16 13:41:40",
+        "latitude": 50.2129789,
+        "longitude": 14.1361922,
+        "title": "Bývalý most a zrušený přejezd v obci Knovíz"
+    },
+    {
+        "photo": "IMG_1453.JPG",
+        "datetime": "2012:09:16 14:41:10",
+        "latitude": 50.2054933,
+        "longitude": 14.1204114,
+        "title": "Pod viaduktem silnice R7 jsem již nenašel žádné stopy, bývala trať se vynoří v polích na okraji obce Jemníky, zde je zachycen sloup osvětlení v bývalé zastávce Jemníky"
+    },
+    {
+        "photo": "IMG_1454.JPG",
+        "datetime": "2012:09:16 15:04:44",
+        "latitude": 50.2016556,
+        "longitude": 14.1124947,
+        "title": "V zářezu ve stopě trati pokračuje mírně stoupající lesní cesta, jedním z pozůstatků je tento propustek"
+    },
+    {
+        "photo": "IMG_1458.JPG",
+        "datetime": "2012:09:16 15:15:06",
+        "latitude": 50.2009389,
+        "longitude": 14.1087353,
+        "title": "Na křížení trati s cestou se nedaleko bývalé stanice Pchery dochovaly kolejnice"
+    },
+    {
+        "photo": "IMG_1457.JPG",
+        "datetime": "2012:09:16 15:16:53",
+        "latitude": 50.2007431,
+        "longitude": 14.1084203,
+        "title": "Budova bývalého nádraží Pchery, nyní v soukromém vlastnictví"
+    },
+    {
+        "photo": "IMG_1462.JPG",
+        "datetime": "2012:09:16 15:23:28",
+        "latitude": 50.2004536,
+        "longitude": 14.1076058,
+        "title": "Zachovalá nakládací rampa nádraží Pchery"
+    },
+    {
+        "photo": "IMG_1463.JPG",
+        "datetime": "2012:09:16 15:33:55",
+        "latitude": 50.2003756,
+        "longitude": 14.1073903,
+        "title": "Betonový pražec v místě bývalého nádraží Pchery, těleso trati pokračuje dále podél silnice a je neprůchodné"
+    },
+    {
+        "photo": "IMG_1465.JPG",
+        "datetime": "2012:09:16 15:52:15",
+        "latitude": 50.19804,
+        "longitude": 14.0987433,
+        "title": "Těleso je opět trochu schůdné u obce Saky, zůstaly zde v tomto úseku betonové pražce i se šrouby"
+    },
+    {
+        "photo": "IMG_1466.JPG",
+        "datetime": "2012:09:16 15:54:13",
+        "latitude": 50.1979506,
+        "longitude": 14.0982439,
+        "title": "Kilometrovník"
+    },
+    {
+        "photo": "IMG_1467.JPG",
+        "datetime": "2012:09:16 15:55:23",
+        "latitude": 50.1977425,
+        "longitude": 14.0975194,
+        "title": "Mladé stromky roustoucí mezi pražci"
+    },
+    {
+        "photo": "IMG_1468.JPG",
+        "datetime": "2012:09:16 16:01:35",
+        "latitude": 50.1971075,
+        "longitude": 14.0962317,
+        "title": "Dobře zachovalý propustek"
+    },
+    {
+        "photo": "IMG_1470.JPG",
+        "datetime": "2012:09:16 16:10:04",
+        "latitude": 50.1966472,
+        "longitude": 14.0957219,
+        "title": "Dobře patrné zaasfaltované kolejnice na přejezdu v obci Saky, u kterého se nacházela i zastávka (pohled směr Podlešín)"
+    },
+    {
+        "photo": "IMG_1471.JPG",
+        "datetime": "2012:09:16 17:01:46",
+        "latitude": 50.1844567,
+        "longitude": 14.0775186,
+        "title": "Zde za obcí Třebichovice končí zrušený úsek tratě a začíná vlečka"
+    },
+    {
+        "photo": "IMG_1472.JPG",
+        "datetime": "2012:09:16 17:05:20",
+        "latitude": 50.1831122,
+        "longitude": 14.0764028,
+        "title": "Vlečka pokračuje do areálu Stavební obnovy železnic"
+    },
+    {
+        "photo": "IMG_1474.JPG",
+        "datetime": "2012:09:16 17:09:30",
+        "latitude": 50.1820286,
+        "longitude": 14.0762925,
+        "title": "Budova bývalé stanice Vinařice v oploceném areálu Stavební obnovy železnic"
+    },
+    {
+        "photo": "IMG_1476.JPG",
+        "datetime": "2012:09:16 17:28:55",
+        "latitude": 50.1780944,
+        "longitude": 14.0742692,
+        "title": "Koleje vlečky SOŽ ve směru Kladno Dubí, podle stavu není vlečka příliš často využívána"
+    },
+    {
+        "photo": "IMG_1477.JPG",
+        "datetime": "2012:09:16 17:29:28",
+        "latitude": 50.1786236,
+        "longitude": 14.0743464,
+        "title": "Ještě jeden pohled zpátky na vjezd do areálu SOŽ od Kladna"
+    }
+]
