b161b713136040932f2f65d76dcd7ed0b1d41451
Jan Prachař <jan.prachar@gmail.com>
2013-09-04 00:37:50 +0200

    Přidána Rakouská Severozápadní v Praze

---
+++ Rakouská Severozápadní Praha/data.js
@@ -0,0 +1,191 @@
+data = [
+    {
+        "photo": "116.JPG",
+        "width": 800,
+        "height": 595,
+        "datetime": "16.10.2010",
+        "latitude": 50.09195,
+        "longitude": 14.4365186,
+        "title": "Nádraží Praha Těšnov (autor neznámý, 1970)"
+    },
+    {
+        "photo": "118.JPG",
+        "width": 800,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.0921325,
+        "longitude": 14.4365636,
+        "title": "Běžný den na Těšnově (Jan Novotný, 1947)"
+    },
+    {
+        "photo": "125.JPG",
+        "width": 800,
+        "height": 586,
+        "datetime": "16.10.2010",
+        "latitude": 50.0939494,
+        "longitude": 14.4407942,
+        "title": "Koleje k těšnovskému nádraží pod Negrelliho viaduktem (Zdeněk Bauer, 1969)"
+    },
+    {
+        "photo": "130.JPG",
+        "width": 707,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.0954508,
+        "longitude": 14.4545886,
+        "title": "Pobřežní ulice, kde býval přístav a nákladové nádraží (autor neznámý, 1920)"
+    },
+    {
+        "photo": "132.JPG",
+        "width": 800,
+        "height": 538,
+        "datetime": "16.10.2010",
+        "latitude": 50.0957053,
+        "longitude": 14.4544442,
+        "title": "Místo kde býval most přes bývalé přístavní rameno Vltavy v Karlíně (Zdeněk Bauer, 1972)"
+    },
+    {
+        "photo": "135.JPG",
+        "width": 553,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.0955394,
+        "longitude": 14.4574344,
+        "title": "Dosud je v Karlíně podél Sokolovské ulice zarostlý násep bývalé trati (Zdeněk Bauer, 1972)"
+    },
+    {
+        "photo": "136.JPG",
+        "width": 800,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.097775,
+        "longitude": 14.4609519,
+        "title": "Pláň před Invalidovnou a hotelem Olympic (Zdeněk Bauer, 1972)"
+    },
+    {
+        "photo": "IMG_1856 byvaly strazni domek v Libni.JPG",
+        "width": 759,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.1007656,
+        "longitude": 14.4672997,
+        "title": "Bývalý strážní domek v Libni"
+    },
+    {
+        "photo": "138.JPG",
+        "width": 800,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.1028275,
+        "longitude": 14.4722497,
+        "title": "Značně zpustlé okolí bývalé stanice Praha Libeň, dolní nádraží (Ondřej Řepka, 1976)"
+    },
+    {
+        "photo": "145.JPG",
+        "width": 800,
+        "height": 534,
+        "datetime": "16.10.2010",
+        "latitude": 50.1054336,
+        "longitude": 14.4797094,
+        "title": "Ulice Na Žertvách vypadala zcela jinak (Ondřej Řepka, 1965)"
+    },
+    {
+        "photo": "149.JPG",
+        "width": 800,
+        "height": 588,
+        "datetime": "16.10.2010",
+        "latitude": 50.1059583,
+        "longitude": 14.4818994,
+        "title": "Křížení s turnovskou tratí (z Hlavního nádraží do Vysočan) u Balabenky, foceno směrem na západ (Zdeněk Bauer, 1972)"
+    },
+    {
+        "photo": "150.JPG",
+        "width": 706,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.1053344,
+        "longitude": 14.4794361,
+        "title": "Křížení s turnovskou tratí u Balabenky, foceno směrem na východ (Zdeněk Bauer, 1972)"
+    },
+    {
+        "photo": "158.JPG",
+        "width": 784,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.1087586,
+        "longitude": 14.4841522,
+        "title": "Z most přes Rokytku zbyly pilíře (Ondřej Řepka, 1968)"
+    },
+    {
+        "photo": "IMG_1831.JPG",
+        "width": 450,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.1087275,
+        "longitude": 14.4837311,
+        "title": "Jižní pilíř mostu přes Rokytku"
+    },
+    {
+        "photo": "IMG_1834 teleso drahy Vysocany.JPG",
+        "width": 450,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.1085981,
+        "longitude": 14.4837753,
+        "title": "Těleso dráhy ve Vysočanech"
+    },
+    {
+        "photo": "IMG_1837.JPG",
+        "width": 800,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.1081853,
+        "longitude": 14.4834242,
+        "title": "Těleso dráhy ve Vysočanech"
+    },
+    {
+        "photo": "IMG_1838 zbytky viaduktu pres Rokytku.JPG",
+        "width": 428,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.1087972,
+        "longitude": 14.4834822,
+        "title": "Severní pilíř mostu před Rokytku"
+    },
+    {
+        "photo": "IMG_1851.JPG",
+        "width": 800,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.1025128,
+        "longitude": 14.4715172,
+        "title": "Praha Libeň dolní nádraží"
+    },
+    {
+        "photo": "IMG_1855.JPG",
+        "width": 800,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.1008933,
+        "longitude": 14.4674731,
+        "title": "Násep přerušený ulicí Švábky"
+    },
+    {
+        "photo": "IMG_1869.JPG",
+        "width": 800,
+        "height": 600,
+        "datetime": "16.10.2010",
+        "latitude": 50.0954106,
+        "longitude": 14.4472269,
+        "title": "Bývalé nádražní budovy nákladového nádraží na Rohanském ostrově"
+    },
+    {
+        "photo": "IMG_1990 byvale drazni budovy na Rohanskem ostrove.JPG",
+        "width": 800,
+        "height": 417,
+        "datetime": "16.10.2010",
+        "latitude": 50.0938789,
+        "longitude": 14.4483608,
+        "title": "Bývalé nádražní budovy nákladového nádraží na Rohanském ostrově"
+    }
+]
